11075e8f8ec17cfa4d9c67919511ed6b6c5f3114
Anthony Mandra <amandra@gmail.com>
2019-04-02 20:03:03 -0400

    generate auth token

--- app/src/main/java/com/anthonymandra/cloudprovider/app/MainActivity.kt
+++ app/src/main/java/com/anthonymandra/cloudprovider/app/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.anthonymandra.cloudprovider.app
 
 import android.content.ComponentName
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.dropbox.core.DbxRequestConfig
+import com.dropbox.core.android.Auth
 import com.dropbox.core.v2.DbxClientV2
 import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 import java.util.concurrent.Executors
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,31 +26,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        Single.fromCallable {
-            val config = DbxRequestConfig.newBuilder(authority).build()
-            val client = DbxClientV2(config, "") // FIXME: REMOVE
-            val account = client.users().currentAccount
-            Log.d("cloud_test", account.name.displayName)
-            var rootFolders = client.files().listFolder("")
-            while (true) {
-                for (metadata in rootFolders.entries) {
-                    Log.d("cloud_path", metadata.pathLower)
-                }
-
-                if (!rootFolders.hasMore) {
-                    break
-                }
-
-                Log.d("cloud_path", "continue")
-                rootFolders = client.files().listFolderContinue(rootFolders.cursor)
-            }
+        loginButton.setOnClickListener {
+            Auth.startOAuth2Authentication(this, "tnw5ufssav0syht")
         }
-            .subscribeOn(Schedulers.from(Executors.newSingleThreadExecutor()))
-            .subscribe()
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            Single.fromCallable {
+                Auth.getOAuth2Token()?.let {
+                    DropboxClientFactory.init(this, it)
+                }
+
+                val client = DropboxClientFactory.client
+                val account = client.users().currentAccount
+                Log.d("cloud_test", account.name.displayName)
+                var rootFolders = client.files().listFolder("")
+                while (true) {
+                    for (metadata in rootFolders.entries) {
+                        Log.d("cloud_path", metadata.pathLower)
+                    }
+
+                    if (!rootFolders.hasMore) {
+                        break
+                    }
+
+                    Log.d("cloud_path", "continue")
+                    rootFolders = client.files().listFolderContinue(rootFolders.cursor)
+                }
+
+//            // Upload "test.txt" to Dropbox
+//            FileInputStream("test.txt").use { file ->
+//                val metadata = client.files().uploadBuilder("/test.txt")
+//                    .uploadAndFinish(file)
+//            }
+            }
+                .subscribeOn(Schedulers.from(Executors.newSingleThreadExecutor()))
+                .subscribe()
         }
     }
 
@@ -67,16 +80,32 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private val authority: String   //TODO: Singleton this or something
-        get() {
-            var authority = "com.anthonymandra.cloudprovider.DropboxProvider"
+    /**
+     * Singleton instance of [DbxClientV2] and friends
+     */
+    object DropboxClientFactory {
 
-            try {
-                val componentName = ComponentName(this, this::class.java.name)
-                val providerInfo = this.packageManager.getProviderInfo(componentName, 0)
-                authority = providerInfo.authority
-            } finally {
-                return authority
+        private var sDbxClient: DbxClientV2? = null
+
+        val client: DbxClientV2
+            get() {
+                return sDbxClient ?: throw IllegalStateException("Client not initialized.")
+            }
+
+        fun init(context: Context, accessToken: String) {
+            if (sDbxClient == null) {
+                var authority = "com.anthonymandra.cloudprovider.DropboxProvider"
+                try {
+                    val componentName = ComponentName(context, this::class.java.name)
+                    val providerInfo = context.packageManager.getProviderInfo(componentName, 0)
+                    authority = providerInfo.authority
+                } catch(e: Exception) {}
+
+                val requestConfig = DbxRequestConfig.newBuilder(authority)
+                    .build()
+
+                sDbxClient = DbxClientV2(requestConfig, accessToken)
             }
         }
+    }
 }
